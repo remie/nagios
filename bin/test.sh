047d9570a843528b25ba6242eac56f5c0dc60438
Remie Bolte <r.bolte@gmail.com>
2018-05-24 10:36:57 +0200

    Code cleanup

--- bin/test.sh
+++ bin/test.sh
@@ -7,4 +7,3 @@ npm run build;
 (cd example && ./node_modules/.bin/gulp)
 node ./bin/nagios-cli compile -f ./example/build/
 docker run --rm -v "$BASEDIR/config/:/config" remie/docker-nagios /usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
-docker run --rm --name nagios -v "$BASEDIR/config/:/config" -p 5000:80 remie/docker-nagios
