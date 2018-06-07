c4fd714d1f5f4091522eab18888d24e84b5c7a71
Remie Bolte <r.bolte@gmail.com>
2018-06-07 09:43:18 +0200

    Check commands for contacts should be properties on the object

--- src/objects/contact.ts
+++ src/objects/contact.ts
@@ -1,9 +1,11 @@
 
-import { Contact } from '../types';
+import { Contact, Check } from '../types';
 import { AbstractInheritableNagiosObj, ObjectType } from './index';
 
 export abstract class ContactObj extends AbstractInheritableNagiosObj {
   configuration: Contact;
+  serviceNotificationCommand?: Check;
+  hostNotificationCommand?: Check;
 
   constructor() {
     super();
