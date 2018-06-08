7bc3afd0d3380a6021f3aa83f29382cfc1c1bd5d
Remie Bolte <r.bolte@gmail.com>
2018-06-08 10:47:41 +0200

    Hostgroup only has hostgroup_name as a required property

--- src/types/hostgroup.ts
+++ src/types/hostgroup.ts
@@ -3,10 +3,10 @@ import { InheritableObjectDefinition } from './ObjectDefinition';
 
 export interface HostGroup extends InheritableObjectDefinition {
   hostgroup_name: string;
-  alias: string;
-  members: string;
-  hostgroup_members: string;
-  notes: string;
-  notes_url: string;
-  action_url: string;
+  alias?: string;
+  members?: string;
+  hostgroup_members?: string;
+  notes?: string;
+  notes_url?: string;
+  action_url?: string;
 }
