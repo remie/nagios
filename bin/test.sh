1f37b150563ec60095c54ec7296a0336c2c43407
Remie Bolte <r.bolte@gmail.com>
2019-04-14 21:50:28 +0200

    Version bump (1.0.0) and dependency cleanup

--- bin/test.sh
+++ bin/test.sh
@@ -4,6 +4,6 @@ BASEDIR=`pwd`;
 
 rm -rf $BASEDIR/config
 npm run build;
-(cd example && npm install && npm link @remie/nagios-cli && ./node_modules/.bin/gulp)
+(cd example && npm install && npm link @remie/nagios-cli && npm run build)
 node ./bin/nagios-cli compile -f ./example/build/
 docker run --rm -v "$BASEDIR/config/:/config" remie/docker-nagios /usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
